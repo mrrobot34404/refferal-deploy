fd77959fd18505c44e5f44e73dfeb6db51a4577b
fawad404 <fawadanxari31@gmail.com>
2024-07-26 10:08:04 -0700

    adding-nav-home

--- app/page.js
+++ app/page.js
@@ -1,6 +1,12 @@
+import Footer from "./components/footer/Footer";
+import Navbar from "./components/navbar/Navbar";
 
 export default function Home() {
   return (
+    <>
+    <Navbar />
     <h1>hello</h1>
+    <Footer />
+    </>
   );
 }
