96129bb144272f867bfd3ee45697d5622b92351f
fawad404 <fawadanxari31@gmail.com>
2024-08-01 11:19:55 -0700

    adding-forgot-password

--- app/forgot-password/page.js
+++ app/forgot-password/page.js
@@ -2,6 +2,8 @@
 import { useState } from 'react';
 import { auth } from '../firebase';
 import { sendPasswordResetEmail } from "firebase/auth";
+import Navbar from '../components/navbar/Navbar';
+import Footer from '../components/footer/Footer';
 
 export default function ForgotPassword() {
   const [email, setEmail] = useState('');
@@ -12,6 +14,7 @@ export default function ForgotPassword() {
 
   return (
     <>
+    <Navbar />
     <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <img
@@ -19,7 +22,7 @@ export default function ForgotPassword() {
             src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
             alt="Your Company"
           />
-          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white">
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-800">
             Forgot Password
           </h2>
         </div>
@@ -27,7 +30,7 @@ export default function ForgotPassword() {
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
           <div className="space-y-6">
             <div>
-              <label htmlFor="email" className="block text-sm font-medium leading-6 text-white">
+              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-800">
                 Email address
               </label>
               <div className="mt-2">
@@ -38,7 +41,7 @@ export default function ForgotPassword() {
                   autoComplete="email"
                   onChange={(e) => setEmail(e.target.value)}
                   required
-                  className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                  className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-gray-800 shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
@@ -47,7 +50,7 @@ export default function ForgotPassword() {
               <button
                 onClick={() => resetEmail()}
                 disabled={!email}
-                className="disabled:opacity-40 flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+                className="disabled:opacity-40 flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm font-semibold leading-6 text-gray-800 shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
               >
                 Send Forgot Password Email
               </button>
@@ -55,6 +58,7 @@ export default function ForgotPassword() {
           </div>
         </div>
       </div>
+      <Footer />
     </>
   )
 }
